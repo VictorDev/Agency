07aad86241057e03a64d614baf48d333fcd57e83
VictorDev <nirvana01_01@mail.ru>
2020-02-04 15:00:06 +0300

    create LoadManager

---
+++ Agentstvo/LoadManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Agentstvo
+{
+    class LoadManager
+    {
+        FileInfo file;
+        StreamReader sr;
+        public LoadManager(String filename)
+        {
+            file = new FileInfo(filename + ".txt");
+            //file.CreateText().Close();
+        }
+
+        public void ReadObject(IReadableObject obj)
+        {
+            
+            obj.Read(this);
+        }
+    }
+}
